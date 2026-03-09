50b0f8b0c3d928db5a38746ac8322c6c66e1cc1f
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2026-03-09 09:58:36 -0400

    added class properties to faker factory

--- tests/factories.py
+++ tests/factories.py
@@ -31,4 +31,9 @@ class ProductFactory(factory.Factory):
         model = Product
 
     id = factory.Sequence(lambda n: n)
+    name = factory.FuzzyChoice(choices=["Hat","Pants","Shirt","Apple","Banana","Pots","Towels","Ford","Chevy","Hammer","Wrench"])
+    description = factory.Faker("text")
+    price = factory.FuzzyDecimal(choices=[0.5, 2000.0, 2])
+    available = factory.FuzzyChoice(choices=[True, False])
+    category = factory.FuzzyChoice(choices=[Category.UNKNOWN,Category.CLOTHS,Category.FOOD,Category.HOUSEWARES,Category.AUTOMOTIVE,Category.TOOLS])
    ## Add code to create Fake Products 
