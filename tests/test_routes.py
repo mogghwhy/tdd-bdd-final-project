be6834ab10fb77f3dc4d61768960cacd974d401f
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2026-03-10 05:01:33 -0400

    add test_get_product test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -167,6 +167,15 @@ class TestProductRoutes(TestCase):
     # ADD YOUR TEST CASES HERE
     #
 
+    def test_get_product(self):
+        """ It should get a product """
+        test_product = self._create_products(1)[0]
+        product_id = test_product.id
+        response = self.client.get(f"{BASE_URL}/{product_id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data['name'], test_product.name)
+
     ######################################################################
     # Utility functions
     ######################################################################
